9231d672eeb78288aeed5bd13cb573f143d13e14
Dapeng Li <luislee818@gmail.com>
2019-05-18 09:07:20 +0200

    feature: add login action for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  def login
+    user = User.find_or_create_by(username: params[:username])
+    render json: user, status: :ok
+  end
+
   def show
     user = User.find(params[:id])
-    render json: { user: user }, status: :ok
+    render json: user, status: :ok
   end
 end
