8cf7765fd4e4048e0311e7955abd3c391f878f96
Dapeng Li <luislee818@gmail.com>
2019-05-19 08:52:55 +0200

    chore: add specs for ClockEntry model

--- app/models/clock_entry.rb
+++ app/models/clock_entry.rb
@@ -1,5 +1,22 @@
 class ClockEntry < ApplicationRecord
   belongs_to :user
 
+  ALLOWED_ACTION_TYPES = %w(IN OUT)
+  DATETIME_GRACE_PERIOD = 5.minutes  # in case user's clock is not synced (not likely, but just in case)
+
+  validates :action_type,
+    inclusion: { in: ALLOWED_ACTION_TYPES,
+                 message: "%{value} is not supported, must be one of #{ALLOWED_ACTION_TYPES}" }
+  validates :datetime, presence: true
+  validate :datetime_not_in_future, if: -> { datetime }
+
   scope :by_date_desc, -> { order(datetime: :desc) }
+
+  private
+
+  def datetime_not_in_future
+    if datetime > DateTime.now + DATETIME_GRACE_PERIOD
+      errors.add(:datetime, 'cannot be in the future')
+    end
+  end
 end
--- spec/models/clock_entry_spec.rb
+++ spec/models/clock_entry_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ClockEntry, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { User.new }
+  subject { described_class.new(user: user) }
+
+  it 'is valid with valid attributes' do
+    subject.action_type = 'IN'
+    subject.datetime = DateTime.now
+
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid when action_type is missing' do
+    subject.datetime = DateTime.now
+
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid when action_type is not supported' do
+    subject.action_type = 'FOO'
+    subject.datetime = DateTime.now
+
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid when datetime is missing' do
+    subject.action_type = 'IN'
+
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid when datetime is in the future' do
+    subject.action_type = 'IN'
+    subject.datetime = DateTime.now + 6.minutes
+
+    expect(subject).to_not be_valid
+  end
 end
